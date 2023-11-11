5adc6eb204489129a1847c616090b285d1faeaa6
ilearner777 <ilearner@protonmail.com>
2023-11-11 07:18:56 -0500

    Clean up repository and prepare for new upstream download

--- delete-repo.ps1
+++ delete-repo.ps1
@@ -1,4 +1,5 @@
 # PowerShell
-Write-Output "Resetting repository..."
+Write-Output "Preparing repository for new upstream download ..."
 Remove-Item -Path "./.github", "./.husky", "./.vscode", "./data", "./docs", "./infra", "./packages", "./tools"."./dist", "./node_modules", "./tmp" -Recurse -Force
-Write-Output "Repository reset."
+Remove-Item -Path "./.editorconfig", "./.eslintignore", "./.eslintrc.json", "./.lintstagedrc.cjs", "./.nvmrc", "./.gitignore", "./.prettierignore", "./.prettierrc", "./babel.config.json", "./nx.json", "./package-lock.json", "./package.json", "./README.md", "./tsconfig.base.json", "./Vagrantfile", "./vercel.json"  -Force
+Write-Output "Repository cleaned out."
